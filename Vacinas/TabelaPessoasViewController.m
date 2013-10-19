1aa075685fb118ec8ff82a6e1c17a68cdd185d3a
diegoaribeiro <daribeiro@MacBook-Pro-de-Diego.local>
2013-10-19 17:12:28 -0300

    coloquei os botoes adicionar e editar

--- Vacinas/TabelaPessoasViewController.m
+++ Vacinas/TabelaPessoasViewController.m
@@ -68,7 +68,7 @@
     NSString *arquivo = [[NSBundle mainBundle] pathForResource:@"bebe" ofType:@"jpg"];
     self.pessoas = [[NSMutableArray alloc]initWithCapacity:20];
     
-    NSLog(@"teste git");
+    NSLog(@"teste gi");
     
     NSData *foto = [NSData dataWithContentsOfFile:arquivo];
     
